f9d4e6078cd8209da4d90d6a2600be6b9c75061c
Hao <xiayushan2003@gmail.com>
2020-12-27 13:30:22 -0800

    Update lab7.py

--- Lab07/lab7.py
+++ Lab07/lab7.py
@@ -1,1 +1,208 @@
+def add_this_many(x, el, lst):
+    """ Adds el to the end of lst the number of times x occurs in lst.
+    >>> lst = [1, 2, 4, 2, 1]
+    >>> add_this_many(1, 5, lst)
+    >>> lst [1, 2, 4, 2, 1, 5, 5]
+    >>> add_this_many(2, 2, lst)
+    >>> lst [1, 2, 4, 2, 1, 5, 5, 2, 2]
+    """
+    if x in lst:
+        count = lst.count(x)
+        for i in range(count):
+            lst.append(el)
+        return lst
 
+
+def group_by(s, fn):
+    """
+    >>> group_by([12, 23, 14, 45], lambda p: p // 10)
+    {1: [12, 14], 2: [23], 4: [45]}
+    >>> group_by(range(-3, 4), lambda x: x * x)
+    {0: [0], 1: [-1, 1], 4: [-2, 2], 9: [-3, 3]}
+    """
+    group_up = {}
+    for x in s:
+        key = fn(x)
+        if key in group_up:
+            group_up[key].append(x)
+        else:
+            group_up[key] = [x]
+    return group_up
+
+
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    times = 0
+    def adder(i):
+        nonlocal times
+        times += 1
+        return n + i + times - 1
+    return adder
+
+
+def memory(n): 
+    """
+    >>> f = memory(10)
+    >>> f(lambda x: x * 2) 
+    20 
+    >>> f(lambda x: x - 7) 
+    13 
+    >>> f(lambda x: x > 5) 
+    True 
+    """
+    def update(g):
+        nonlocal n
+        n = g(n)
+        return n
+    return update
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    """
+    previous, current = 0, 1
+    def fib():
+        nonlocal previous, current
+        original_previous = previous
+        previous, current = current, current + previous
+        return original_previous
+    return fib
+
+
+def make_withdraw1(balance):
+    """Return a withdraw function with BALANCE as its starting balance.
+    >>> withdraw = make_withdraw(1000)
+    >>> withdraw(100)
+    900
+    >>> withdraw(100)
+    800
+    >>> withdraw(900)
+    'Insufficient funds'
+    """
+    def withdraw(amount):
+        nonlocal balance
+        if amount > balance:
+           return 'Insufficient funds'
+        balance = balance - amount
+        return balance
+    return withdraw
+
+
+def make_withdraw(balance, password):
+    """Return a password-protected withdraw function.
+    >>> w = make_withdraw(100, 'hax0r')
+    >>> w(25, 'hax0r')
+    75
+    >>> error = w(90, 'hax0r')
+    >>> error
+    'Insufficient funds'
+    >>> error = w(25, 'hwat')
+    >>> error
+    'Incorrect password'
+    >>> new_bal = w(25, 'hax0r')
+    >>> new_bal
+    50
+    >>> w(75, 'a')
+    'Incorrect password'
+    >>> w(10, 'hax0r')
+    40
+    >>> w(20, 'n00b')
+    'Incorrect password'
+    >>> w(10, 'hax0r')
+    "Your account is locked. Attempts: ['hwat', 'a', 'n00b']"
+    >>> w(10, 'l33t')
+    "Your account is locked. Attempts: ['hwat', 'a', 'n00b']"
+    >>> type(w(10, 'l33t')) == str
+    True
+    """
+    wrong_password_list = []
+    def withdraw(amount, pass_word):
+        nonlocal balance
+        if len(wrong_password_list) == 3:
+            return "Your account is locked. Attempts: {}".format(wrong_password_list)
+        elif pass_word != password:
+            wrong_password_list.append(pass_word)
+            return "Incorrect password"
+        elif amount > balance:
+            return 'Insufficient funds'
+        else:
+            balance -= amount
+            return balance
+    return withdraw
+
+
+def make_joint(withdraw, old_password, new_password):
+    """Return a password-protected withdraw function that has joint access to the balance of withdraw.
+
+    >>> w = make_withdraw(100, 'hax0r')
+    >>> w(25, 'hax0r')
+    75
+    >>> make_joint(w, 'my', 'secret')
+    'Incorrect password'
+    >>> j = make_joint(w, 'hax0r', 'secret')
+    >>> w(25, 'secret')
+    'Incorrect password'
+    >>> j(25, 'secret')
+    50
+    >>> j(25, 'hax0r')
+    25
+    >>> j(100, 'secret')
+    'Insufficient funds'
+
+    >>> j2 = make_joint(j, 'secret', 'code')
+    >>> j2(5, 'code')
+    20
+    >>> j2(5, 'secret')
+    15
+    >>> j2(5, 'hax0r')
+    10
+
+    >>> j2(25, 'password')
+    'Incorrect password'
+    >>> j2(5, 'secret')
+    "Your account is locked. Attempts: ['my', 'secret', 'password']"
+    >>> j(5, 'secret')
+    "Your account is locked. Attempts: ['my', 'secret', 'password']"
+    >>> w(5, 'hax0r')
+    "Your account is locked. Attempts: ['my', 'secret', 'password']"
+    >>> make_joint(w, 'hax0r', 'hello')
+    "Your account is locked. Attempts: ['my', 'secret', 'password']"
+    """
+    verification = withdraw(0, old_password)
+    if type(verification) not in [int, float]:
+        return verification
+
+    def account(amount, password):
+        if password == new_password:
+            password = old_password
+        return withdraw(amount, password)
+    return account
